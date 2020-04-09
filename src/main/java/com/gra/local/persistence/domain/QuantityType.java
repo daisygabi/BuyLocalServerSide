c9ea5bd8ac40138f47792e86a0b39de99af41fe7
Gabi <gabrielaradu.developer@gmail.com>
2020-04-09 21:52:28 +0300

    Save product first version.

---
+++ src/main/java/com/gra/local/persistence/domain/QuantityType.java
@@ -0,0 +1,35 @@
+package com.gra.local.persistence.domain;
+
+import org.springframework.security.core.GrantedAuthority;
+
+public enum QuantityType implements GrantedAuthority {
+    GRAMS("Grams", 0),
+    KG("Kg", 1),
+    Miligrams("ml", 2),
+    Liter("L", 3);
+
+    private String name;
+    private int index;
+
+    QuantityType(String name, int index) {
+        this.name = name;
+        this.index = index;
+    }
+
+    public String getAuthority() {
+        return name();
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public static QuantityType getByTypeId(int typeId) {
+        for (QuantityType value : values()) {
+            if (value.getIndex() == typeId) {
+                return value;
+            }
+        }
+        return null;
+    }
+}
--- src/main/java/com/gra/local/persistence/services/VendorProductService.java
+++ src/main/java/com/gra/local/persistence/services/VendorProductService.java
@@ -1,15 +1,27 @@
 package com.gra.local.persistence.services;
 
+import com.gra.local.persistence.EntityHelper;
+import com.gra.local.persistence.domain.VendorProduct;
 import com.gra.local.persistence.repositories.VendorProductRepository;
+import com.gra.local.persistence.services.dtos.VendorProductDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class VendorProductService {
 
-    @Autowired
     private VendorProductRepository vendorProductRepository;
 
-    public VendorProductService() {
+    @Autowired
+    public void setUserRepository(VendorProductRepository vendorProductRepository) {
+        this.vendorProductRepository = vendorProductRepository;
+    }
+
+    public VendorProduct save(VendorProductDto dto) {
+        return getVendorProductRepository().save(EntityHelper.convertToAbstractEntity(dto, VendorProduct.class));
+    }
+
+    public VendorProductRepository getVendorProductRepository() {
+        return vendorProductRepository;
     }
 }
