fa9ed35895e388a671843bbee0759255deac9e07
Gabi <gabrielaradu.developer@gmail.com>
2020-04-09 16:36:03 +0300

    Initialise twilio java sdk.

--- src/main/java/com/gra/local/BuyLocalApplication.java
+++ src/main/java/com/gra/local/BuyLocalApplication.java
@@ -1,5 +1,6 @@
 package com.gra.local;
 
+import com.twilio.Twilio;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,9 +12,14 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class BuyLocalApplication {
 
     @Bean
-    public ModelMapper modelMapper() { return new ModelMapper(); }
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(BuyLocalApplication.class, args);
+
+        // Initialize Twilio to be able to use in throughout the application
+        Twilio.init(System.getenv("TWILIO_ACCOUNT_SID"), System.getenv("TWILIO_AUTH_SECRET"));
     }
 }
