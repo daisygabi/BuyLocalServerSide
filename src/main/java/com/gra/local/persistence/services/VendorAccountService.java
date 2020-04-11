f7fa98d14c75526abd812bac007557d414e79f95
Gabi <gabrielaradu.developer@gmail.com>
2020-04-11 21:06:02 +0300

    Verify vendor by sending sms with code. WIP

--- src/main/java/com/gra/local/persistence/services/VendorAccountService.java
+++ src/main/java/com/gra/local/persistence/services/VendorAccountService.java
@@ -1,27 +1,58 @@
 package com.gra.local.persistence.services;
 
+import com.gra.local.exceptions.CustomException;
 import com.gra.local.persistence.EntityHelper;
 import com.gra.local.persistence.domain.VendorAccount;
 import com.gra.local.persistence.repositories.VendorAccountRepository;
 import com.gra.local.persistence.services.dtos.VendorAccountDto;
+import com.twilio.rest.lookups.v1.PhoneNumber;
+import com.twilio.rest.lookups.v1.PhoneNumberFetcher;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 @Service
 public class VendorAccountService {
 
     private VendorAccountRepository vendorAccountRepository;
+    private TwilioSmsApiWrapper twilioSmsApiWrapper;
+
+    public VendorAccount save(VendorAccountDto dto) {
+        return getVendorAccountRepository().save(EntityHelper.convertToAbstractEntity(dto, VendorAccount.class));
+    }
+
+    public boolean validatePhoneNumber(String phoneNumber) {
+        try {
+            PhoneNumberFetcher fetcher = getTwilioSmsApiWrapper().createPhoneNumberFetcher(phoneNumber);
+            PhoneNumber number = fetcher.fetch();
+            return number.getCarrier() != null;
+        } catch (com.twilio.exception.ApiException e) {
+            throw new CustomException("Couldn't validate phone number", HttpStatus.NOT_FOUND, e);
+        }
+    }
+
+    public boolean sendValidationSMSCodeToVendor(String toNumber, String message) {
+        try {
+            return getTwilioSmsApiWrapper().create(toNumber, System.getenv("TWILIO_DEV_TEST_PHONE_NR"), message);
+        } catch (com.twilio.exception.ApiException e) {
+            throw new CustomException("Couldn't send validation SMS to Vendor", HttpStatus.BAD_REQUEST, e);
+        }
+    }
 
     @Autowired
     public void setVendorAccountService(VendorAccountRepository vendorAccountRepository) {
         this.vendorAccountRepository = vendorAccountRepository;
     }
 
-    public VendorAccount save(VendorAccountDto dto) {
-        return getVendorAccountRepository().save(EntityHelper.convertToAbstractEntity(dto, VendorAccount.class));
-    }
-
     public VendorAccountRepository getVendorAccountRepository() {
         return vendorAccountRepository;
+    }
+
+    public void setTwilioSmsApiWrapper(TwilioSmsApiWrapper twilioSmsApiWrapper) {
+        this.twilioSmsApiWrapper = twilioSmsApiWrapper;
+    }
+
+    public TwilioSmsApiWrapper getTwilioSmsApiWrapper() {
+        return twilioSmsApiWrapper != null ? twilioSmsApiWrapper : getTwilioSmsApiWrapper();
     }
 }
--- src/test/java/com/gra/local/controllers/VendorAccountControllerTest.java
+++ src/test/java/com/gra/local/controllers/VendorAccountControllerTest.java
@@ -6,8 +6,7 @@ import org.junit.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
 
 public class VendorAccountControllerTest extends BasicRequestTest {
 
@@ -22,5 +21,22 @@ public class VendorAccountControllerTest extends BasicRequestTest {
         ResponseEntity<VendorAccount> postResponse = getRestTemplate().postForEntity(getRootUrl() + "/account/", vendorAccountDto, VendorAccount.class);
         assertEquals(HttpStatus.OK, postResponse.getStatusCode());
         assertNotNull(postResponse.getBody());
+    }
+
+    @Test
+    public void testValidatePhoneNumber_usingTwilio_API_shouldReturm_StatusOK() {
+        String phoneNumber = System.getenv("TWILIO_DEV_TEST_PHONE_NR");
+        ResponseEntity<Boolean> postResponse = getRestTemplate().postForEntity(getRootUrl() + "/account/validate/" + phoneNumber, phoneNumber, Boolean.class);
+        assertEquals(HttpStatus.OK, postResponse.getStatusCode());
+        assertTrue(String.valueOf(postResponse), Boolean.TRUE);
+    }
+
+    @Test
+    public void testVerifyAccount_usingTwilio_API_bySendingSMS_shouldReturn_StatusOK() {
+        String to = System.getenv("TWILIO_RECEIVER_TEST_PHONE_NR");
+        String message = "Demo message";
+        ResponseEntity<Boolean> postResponse = getRestTemplate().postForEntity(getRootUrl() + "/account/verifyAccount/" + to, message, Boolean.class);
+        assertEquals(HttpStatus.OK, postResponse.getStatusCode());
+        assertTrue(String.valueOf(postResponse), Boolean.TRUE);
     }
 }
