5912d3fcabf777368c1b4da15d13a3c1a0967155
Gabi <gabrielaradu.developer@gmail.com>
2020-04-21 16:22:47 +0300

    Small refactoring.

--- src/main/resources/dev/migration/afterMigrate.sql
+++ src/main/resources/dev/migration/afterMigrate.sql
@@ -12,37 +12,37 @@ on conflict do nothing;
 /* Create demo dev products*/
 INSERT INTO products (name, min_quantity_per_order, max_quantity_per_order, quantity_type, created_at, updated_at,
                       in_stock, price, vendor_id, currency)
-VALUES ('Bok Choy', 1, 0, 4, '2020-04-16T14:12:39.000+0000', '2020-04-16T14:12:39.000+0000', true, 12, 1, 'EURO')
+VALUES ('Bok Choy', 1, 0, 1, '2020-04-16T14:12:39.000+0000', '2020-04-16T14:12:39.000+0000', true, 12, 1, 'EURO')
 on conflict do nothing;
 
 INSERT INTO products (name, min_quantity_per_order, max_quantity_per_order, quantity_type, created_at, updated_at,
                       in_stock, price, vendor_id, currency)
-VALUES ('Sorrel Leaves', 1, 0, 4, '2020-04-16T14:12:39.000+0000', '2020-04-16T14:12:39.000+0000', true, 12, 1, 'EURO')
+VALUES ('Sorrel Leaves', 1, 0, 2, '2020-04-16T14:12:39.000+0000', '2020-04-16T14:12:39.000+0000', true, 12, 1, 'EURO')
 on conflict do nothing;
 
 INSERT INTO products (name, min_quantity_per_order, max_quantity_per_order, quantity_type, created_at, updated_at,
                       in_stock, price, vendor_id, currency)
-VALUES ('Kaffir Lime', 1, 0, 4, '2020-04-16T14:12:39.000+0000', '2020-04-16T14:12:39.000+0000', true, 12, 1, 'EURO')
+VALUES ('Kaffir Lime', 1, 0, 2, '2020-04-16T14:12:39.000+0000', '2020-04-16T14:12:39.000+0000', true, 12, 1, 'EURO')
 on conflict do nothing;
 
 INSERT INTO products (name, min_quantity_per_order, max_quantity_per_order, quantity_type, created_at, updated_at,
                       in_stock, price, vendor_id, currency)
-VALUES ('Pimiento', 1, 0, 4, '2020-04-16T14:12:39.000+0000', '2020-04-16T14:12:39.000+0000', true, 12, 1, 'EURO')
+VALUES ('Pimiento', 1, 0, 1, '2020-04-16T14:12:39.000+0000', '2020-04-16T14:12:39.000+0000', true, 12, 1, 'EURO')
 on conflict do nothing;
 
 INSERT INTO products (name, min_quantity_per_order, max_quantity_per_order, quantity_type, created_at, updated_at,
                       in_stock, price, vendor_id, currency)
-VALUES ('Mustard Leaves', 1, 0, 4, '2020-04-16T14:12:39.000+0000', '2020-04-16T14:12:39.000+0000', true, 12, 1, 'EURO')
+VALUES ('Mustard Leaves', 1, 0, 0, '2020-04-16T14:12:39.000+0000', '2020-04-16T14:12:39.000+0000', true, 12, 1, 'EURO')
 on conflict do nothing;
 
 ------------------------------------
 
 INSERT INTO products (name, min_quantity_per_order, max_quantity_per_order, quantity_type, created_at, updated_at,
                       in_stock, price, vendor_id, currency)
-VALUES ('Product 1', 1, 0, 4, '2020-04-16T14:12:39.000+0000', '2020-04-16T14:12:39.000+0000', true, 12, 2, 'EURO')
+VALUES ('Product 1', 1, 0, 1, '2020-04-16T14:12:39.000+0000', '2020-04-16T14:12:39.000+0000', true, 12, 2, 'EURO')
 on conflict do nothing;
 
 INSERT INTO products (name, min_quantity_per_order, max_quantity_per_order, quantity_type, created_at, updated_at,
                       in_stock, price, vendor_id, currency)
-VALUES ('Product 2', 1, 0, 4, '2020-04-16T14:12:39.000+0000', '2020-04-16T14:12:39.000+0000', true, 12, 2, 'EURO')
+VALUES ('Product 2', 1, 0, 2, '2020-04-16T14:12:39.000+0000', '2020-04-16T14:12:39.000+0000', true, 12, 2, 'EURO')
 on conflict do nothing;
